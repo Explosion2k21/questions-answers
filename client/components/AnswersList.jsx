f72b455c3b8ef88965b5def97f3f53b71f280360
AliGhanmi <ghanmiali7@gmail.com>
2021-03-02 17:12:05 +0100

    increment helpfulness answer

--- client/components/AnswersList.jsx
+++ client/components/AnswersList.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import moment from 'moment';
+import axios from 'axios';
 
 class AnswersList extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            bolean: true
         }
+        this.onSubmit= this.onSubmit.bind(this)
+    }
+    // this function to update the count of helpfulness for each answer
+    onSubmit(id) {
+        console.log(id)
+        axios.put(`/updateAnswer/${id}`).then((res) => {
+            console.log('res', res)
+            this.setState({ bolean: false })
+        })
+        // calling the fetch function to update dinamically without refreshing the page
+         this.props.fetch()
     }
     
   
@@ -19,7 +32,8 @@ class AnswersList extends React.Component {
            <div className="answers-details">
            <p>by {this.props.answers.answerer_name},</p>
              <p>{moment(this.props.answers.date).format('ll')} | </p>
-             <span > Helpful? <a>Yes({this.props.answers.helpfulness})</a> </span>
+             {this.state.bolean? <div><span > Helpful? <a className="help" onClick={() => { this.onSubmit(this.props.answers.id) }}>Yes({this.props.answers.helpfulness})</a> </span></div>:<div><span > Helpful? <a className="help">Yes({this.props.answers.helpfulness})</a> </span></div>}
+             
            </div>
            
             </div>
