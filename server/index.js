032ae763bdb03b5ac540740715b66fd001773af2
AliGhanmi <ghanmiali7@gmail.com>
2021-03-02 21:13:17 +0100

    refactored answers list style

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 require('dotenv').config();
 // this is a get request to fetch the questions data of the product selected
 app.get("/questions", (req, res) =>{
-  axios.get("https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions?product_id=11002&page=1&count=5", {
+  axios.get("https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/qa/questions?product_id=11048&page=1&count=5", {
       headers: {
         Authorization: process.env.TOKEN,
       }
@@ -32,6 +32,7 @@ app.put("/update/:id", (req, res) => {
     res.send(result)
   })
 })
+//this is a put request to update the count of helpfulness for each answer
 
 app.put("/updateAnswer/:id", (req, res) => {
   console.log('tttt', req.params)
